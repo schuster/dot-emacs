05b414c953ae41880836c5d168213058a68effeb
Jonathan Schuster <jonathan.p.schuster@gmail.com>
2017-04-09 10:07:20 -0400

    Initial import

---
+++ elisp/mode-setup.el
@@ -0,0 +1,17 @@
+(require 'fractL)
+
+;; ParEdit
+(autoload 'paredit-mode "paredit"
+  "Minor mode for pseudo-structurally editing Lisp code." t)
+
+;; File extensions/major mode bindings
+(add-to-list 'auto-mode-alist '("\\.rkt$" . racket-mode))
+(add-to-list 'auto-mode-alist '("\\.transit$" . fractL-mode))
+(add-to-list 'auto-mode-alist '("\\.ls$" . racket-mode))
+(add-to-list 'auto-mode-alist '("\\.ttest$" . racket-mode))
+(add-to-list 'auto-mode-alist '("\\.k$" . k3-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+;; Set up ispell for OSX
+(if (eq system-type 'darwin)
+  (setq ispell-program-name "/usr/local/bin/aspell"))
